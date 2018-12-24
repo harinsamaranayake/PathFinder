6c52c9507c030d8f113a1b6d347bd8b57b53b6ba
harinsamaranayake <harin.ncsl@gmail.com>
2018-12-24 14:56:33 +0530

    updated unit modules

--- ir/ir.ino
+++ ir/ir.ino
@@ -1,40 +1,19 @@
-int l3=2;
-int l2=3;
-int l1=4;
-int m0=5;
-int r1=6;
-int r2=7;
-int r3=8;
+int irPin=2;
 
 void setup() {
-  pinMode(l3,INPUT);
-  pinMode(l2,INPUT);
-  pinMode(l1,INPUT);
-  pinMode(m0,INPUT);
-  pinMode(r1,INPUT);
-  pinMode(r2,INPUT);
-  pinMode(r3,INPUT);
-
+  pinMode(irPin,INPUT); 
+  
   Serial.begin(9600);
 }
 
-void loop() {  
-  //Serial.print(getReading(l3)); 
-  printAll(); 
-  delay(50);
+void loop() {
+  int reading=getReading(irPin);  
+  Serial.print(reading); 
+    
+  delay(1000);
 }
 
 int getReading(int irPin){
   int reading=digitalRead(irPin);
   return reading;
-}
-
-void printAll(){
-  Serial.print(getReading(l3));
-  Serial.print(getReading(l2));  
-  Serial.print(getReading(l1));
-  Serial.print(getReading(m0)); 
-  Serial.print(getReading(r1)); 
-  Serial.print(getReading(r2)); 
-  Serial.println (getReading(r3));  
 }
--- sonar/sonar.ino
+++ sonar/sonar.ino
@@ -1,28 +1,33 @@
 const int trigPin = 30;
 const int echoPin = 32;
-//22 24 | 26 28 | 30 32
 
 void setup() {
+  //pin setup
   pinMode(trigPin, OUTPUT); 
   pinMode(echoPin, INPUT); 
+  
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println(getDistance(trigPin,echoPin));
+  double distance=getDistance(trigPin,echoPin);
+  Serial.println(distance);
+  
   delay(1000);
 }
 
 int getDistance(int trig,int echo){
+  //clear channels
   digitalWrite(trig, LOW);
   delayMicroseconds(2);
 
+  //send signal
   digitalWrite(trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
 
   double duration = pulseIn(echo, HIGH);
-
+  
   int distance= duration*0.034/2;
 
   return distance;
